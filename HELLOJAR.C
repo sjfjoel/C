0f1a9b4100a8ff2990ec7f055487745f5eb02a39
Joel <52481914+sjfjoel@users.noreply.github.com>
2021-05-08 22:30:09 +0530

    welcome program in c

---
+++ HELLOJAR.C
@@ -0,0 +1,10 @@
+//the fisrt program in c
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	//simply printing welcome message
+	clrscr();//used to clear previous output statements from the output screen
+	printf("Hello Jarvis !");
+	getch();// used because im using Turbo C++, to hold the output screen till we enter a enter key
+}
