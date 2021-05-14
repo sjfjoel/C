51f202782fab8b124fcb9c8b9aa25bb5e1de428f
joelFernandez <52481914+sjfjoel@users.noreply.github.com>
2021-05-14 10:53:41 +0530

    Pattern printing

---
+++ PATTERN2.C
@@ -0,0 +1,27 @@
+// Printing star pattern
+/*
+	*
+	* *
+	* * *
+	* * * *
+	* * * * *
+*/
+
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i, j;
+	clrscr();
+
+	for(i=1; i<=5; i++)
+	{
+		for(j=1; j<=i; j++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+
+	getch();
+}
